d6e84319df8856496ef7a45fc06aae0301d2a582
alex <o.lototskyi@gmail.com>
2023-03-27 15:23:02 +0300

    using refs to get data from form and reset it

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -8,14 +8,18 @@ import classes from './AddUser.module.css';
 
 const AddUser = props => {
 
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
     const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
         
-        if (enteredUsername.trim().length === 0 || enteredAge.trim() === 0) {
+        const enteredName = nameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;
+
+        if (enteredName.trim().length === 0 || enteredUserAge.trim() === 0) {
             setError({
                 title: 'Invalid input',
                 message: 'Please enter a valid name and age (non empty values).'
@@ -23,7 +27,7 @@ const AddUser = props => {
             return;
         }
 
-        if (+enteredAge < 1) {
+        if (+enteredUserAge < 1) {
             setError({
                 title: 'Invalid age',
                 message: 'Please enter a valid age (> 0).'
@@ -31,18 +35,13 @@ const AddUser = props => {
             return;
         }
 
-        props.onAddUser(enteredUsername, enteredAge);
-        setEnteredUsername('');
-        setEnteredAge('');
+        props.onAddUser(enteredName, enteredUserAge);
+
+        // not a best solution to update DOM via refs, but in this case just to reset form inputs it's ok
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     };
 
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    };
-    
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    };
 
     const errorHandler = () => {
         setError(null);
@@ -54,9 +53,17 @@ const AddUser = props => {
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
-                    <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+                    <input 
+                        id="username" 
+                        type="text" 
+                        ref={nameInputRef}
+                    />
                     <label htmlFor="age">Age (Years)</label>
-                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+                    <input 
+                        id="age" 
+                        type="number" 
+                        ref={ageInputRef}
+                    />
                     <Button type='submit'>Add User</Button>
                 </form>
             </Card>
